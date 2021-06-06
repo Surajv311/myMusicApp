b6e617367e8560a2c9a50f16d0713db551c1f36b
Surajv <vsuraj2405@gmail.com>
2021-06-06 12:00:44 +0530

    test code

---
+++ client_frontend/src/Login.js
@@ -0,0 +1,10 @@
+import React from "react";
+
+const AUTH_URL =
+  "https://accounts.spotify.com/authorize?client_id=5fe01282e44241328a84e7c5cc169165&response_type=code&redirect_uri=https%3A%2F%2Fexample.com%2Fcallback&scope=user-read-private%20user-read-email&state=34fFs29kd09";
+
+const Login = () => {
+  return <div></div>;
+};
+
+export default Login;
