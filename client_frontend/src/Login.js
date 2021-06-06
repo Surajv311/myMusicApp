37e8bcf11bc4dcc86bfc9a4d0d13ff923dcb8ff1
Surajv <vsuraj2405@gmail.com>
2021-06-06 12:06:12 +0530

    client_id added

--- client_frontend/src/Login.js
+++ client_frontend/src/Login.js
@@ -1,7 +1,7 @@
 import React from "react";
 
 const AUTH_URL =
-  "https://accounts.spotify.com/authorize?client_id=5fe01282e44241328a84e7c5cc169165&response_type=code&redirect_uri=http://localhost:3000&scope=streaming%20user-read-email%20user-read-private%20user-library-read%20user-library-modify%20user-read-playback-state%20user-modify-playback-state";
+  "https://accounts.spotify.com/authorize?client_id=f673f8eb405e48909a2dde629405d4b6&response_type=code&redirect_uri=http://localhost:3000&scope=streaming%20user-read-email%20user-read-private%20user-library-read%20user-library-modify%20user-read-playback-state%20user-modify-playback-state";
 
 const Login = () => {
   return <div></div>;
