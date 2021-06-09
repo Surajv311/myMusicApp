91b9f79e7cc6d9fca84fea86459d78a1f1b538a4
Surajv <vsuraj2405@gmail.com>
2021-06-09 11:39:40 +0530

    dashboard added

--- client_frontend/src/App.js
+++ client_frontend/src/App.js
@@ -5,7 +5,7 @@ const code = new URLSearchParams(window.location.search).get("code");
 // to get the code in the url, to get tokens......url frame: 'code'..
 
 function App() {
-  return <Login />;
+  return code ? <Dashboard code={code} /> : <Login />;
 }
 
 export default App;
