e6849c8bcd151c6112ecf55dcbe76b42423f7d32
Surajv <vsuraj2405@gmail.com>
2021-06-09 11:34:10 +0530

    code to get tokens.....

--- server_backend/server.js
+++ server_backend/server.js
@@ -2,12 +2,14 @@
 
 const express = require("express");
 const SpotifyApi = require("spotify-web-api-node");
+// spotifywebapi would take the code from url to get tokens...
 
 const app = express();
 
 const clientId_ = "CLIENT_CODE";
 const clientSecret_ = "SECRET_CODE";
 
+//docs
 var credentials = {
   //   redirectUri:  'http://localhost:3000/',
   redirectUri: "http://localhost:3000/callback/",
@@ -17,5 +19,18 @@ var credentials = {
 
 app.post("/login", (req, res) => {
   const code = req.body.code;
-  const temp = new SpotifyApi(credentials);
+  const api = new SpotifyApi(credentials);
+  // we get tokens once we authorize the code, in url...
+  api
+    .authorizationCodeGrant(code)
+    .then((data) => {
+      res.json({
+        accessToken: data.body.access_token,
+        refreshToken: data.body.refresh_token,
+        expiresIn: data.body.expires_in,
+      });
+    })
+    .catch((err) => {
+      res.sendStatus(400);
+    });
 });
