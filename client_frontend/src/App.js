3521790578be3b84533f75d83120a0c7f7f95eaa
Surajv <vsuraj2405@gmail.com>
2021-06-09 11:38:30 +0530

    getting code part from url...

--- client_frontend/src/App.js
+++ client_frontend/src/App.js
@@ -1,6 +1,9 @@
 import Login from "./Login.js";
 import "bootstrap/dist/css/bootstrap.min.css";
 
+const code = new URLSearchParams(window.location.search).get("code");
+// to get the code in the url, to get tokens......url frame: 'code'..
+
 function App() {
   return <Login />;
 }
