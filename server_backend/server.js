fe62b14cd73ab337095f0741e80ad022c3f53074
Surajv <vsuraj2405@gmail.com>
2021-06-09 10:29:02 +0530

    setting up login route

--- server_backend/server.js
+++ server_backend/server.js
@@ -1,1 +1,21 @@
 // fixed nodemon update by deleting nodemon.ps1 file... without changing execution policies...
+
+const express = require("express");
+const SpotifyApi = require("spotify-web-api-node");
+
+const app = express();
+
+const clientId_ = "CLIENT_CODE";
+const clientSecret_ = "SECRET_CODE";
+
+var credentials = {
+  //   redirectUri:  'http://localhost:3000/',
+  redirectUri: "http://localhost:3000/callback/",
+  clientId: clientId_, // dotenv...
+  clientSecret: clientSecret_,
+};
+
+app.post("/login", (req, res) => {
+  const code = req.body.code;
+  const spotifyApi = new SpotifyApi(credentials);
+});
