a982d368bc1b8e37a648dedf891906db486eccd1
Surajv <vsuraj2405@gmail.com>
2021-06-12 18:36:48 +0530

    dashboard search created

--- client_frontend/src/Dashboard.js
+++ client_frontend/src/Dashboard.js
@@ -1,9 +1,67 @@
-import React from "react";
+import { React, useState, useEffect } from "react";
 import Auth from "./Auth";
+import TrackSearch from "./TrackSearch";
+import { Container, Form } from "react-bootstrap";
+import SpotifyApi from "spotify-web-api-node";
 
-const Dashboard = ({ code }) => {
+const client_id = "585eaa2fa9ec4891a14bff801067f8d8";
+
+const api_search = new SpotifyApi(client_id);
+
+export default function Dashboard({ code }) {
   const accessToken = Auth(code);
-  return <div>{code}</div>;
-};
+  const [search, setSearch] = useState("");
+  const [searchResults, setSearchResults] = useState([]);
 
-export default Dashboard;
+  useEffect(() => {
+    if (!accessToken) return; // if accessToken doesn't exist then return ...else run useEffect...
+    api_search.setAccessToken(accessToken);
+  }, [accessToken]);
+
+  useEffect(() => {
+    if (!search) return setSearchResults([]);
+    if (!accessToken) return;
+    let cancelFlag = false;
+    api_search.searchTracks(search).then((res) => {
+      if (cancelFlag) return;
+      setSearchResults(
+        res.body.tracks.items.map((track__) => {
+          const smallestAlbumImage = track__.album.images.reduce(
+            (smallImage, image) => {
+              if (image.height < smallImage.height) return image;
+              return smallImage;
+            },
+            track__.album.images[0] // taking the smallest pic for image... in res.body... different images...album pic
+          );
+
+          return {
+            artist: track__.artists[0].name,
+            title: track__.name,
+            uri: track__.uri,
+            albumUrl: smallestAlbumImage.url,
+          };
+        })
+      );
+    });
+
+    return () => (cancelFlag = true); // so when we search for a song, to avoid delay for every alphabet we put...
+    // put a flag...
+  }, [search, accessToken]);
+
+  return (
+    <Container className="d-flex flex-column py-2" style={{ height: "100vh" }}>
+      <Form.Control
+        type="search"
+        placeholder="Search Songs..."
+        value={search}
+        onChange={(e) => setSearch(e.target.value)}
+      />
+      <div className="flex-grow-1 my-2" style={{ overflowY: "auto" }}>
+        {searchResults.map((track) => (
+          <TrackSearch track={track} key={track.uri} />
+        ))}
+      </div>
+      <div></div>
+    </Container>
+  );
+}
