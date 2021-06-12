8956be3f98f82bd92dc49ce5618696a55a6480ad
Surajv <vsuraj2405@gmail.com>
2021-06-12 20:24:43 +0530

    added player... added color.

---
+++ client_frontend/src/Player.js
@@ -0,0 +1,31 @@
+import { useState, useEffect } from "react";
+import SpotifyPlayer from "react-spotify-web-playback"; // docs....
+
+export default function Player({ accessToken, trackUri }) {
+  const [play, setPlay] = useState(false);
+
+  useEffect(() => setPlay(true), [trackUri]);
+
+  if (!accessToken) return; // if no access token then return...
+  return (
+    <SpotifyPlayer
+      styles={{
+        // docs
+        activeColor: "#fff",
+        bgColor: "#333",
+        color: "#fff",
+        loaderColor: "#fff",
+        sliderColor: "#1cb954",
+        trackArtistColor: "#ccc",
+        trackNameColor: "#fff",
+      }}
+      token={accessToken}
+      showSaveIcon
+      callback={(state) => {
+        if (!state.isPlaying) setPlay(false); // callback every time song changes...
+      }}
+      play={play}
+      uris={trackUri ? [trackUri] : []} // it expects uri / empty array...
+    />
+  );
+}
